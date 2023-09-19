d8a539e683bb591e03f0cc982d1aae8196ac74d5
junghope <jungho80234@gmail.com>
2023-09-19 12:46:38 +0900

    op_kwarg 예제 dag 업로드

--- dags/dags_python_with_op_kwargs.py
+++ dags/dags_python_with_op_kwargs.py
@@ -15,8 +15,8 @@ with DAG(
     regist2_t1 = PythonOperator(
         task_id = 'regist2_t1',
         python_callable=regist2,
-        op_args = ['jhlee', 'man', 'kr', 'seoul']
-        op_kwargs= ['email':'junhgo80234@gamil.com', 'phone':'010-']
+        op_args = ['jhlee', 'man', 'kr', 'seoul'],
+        op_kwargs= {'email':'junhgo80234@gamil.com', 'phone':'010-'}
     )
 
     regist2_t1
